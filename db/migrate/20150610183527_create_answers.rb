8a86efe24a2c5e3cca72d81da83ef21113a581fe
mBabbitt <meganbabbitt23@gmail.com>
2015-06-10 15:00:04 -0700

    assignment 30 completed

--- db/migrate/20150610183527_create_answers.rb
+++ db/migrate/20150610183527_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.body :text
+      t.text :body
       t.references :question, index: true, foreign_key: true
 
       t.timestamps null: false
