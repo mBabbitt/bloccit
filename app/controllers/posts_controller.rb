d8d7feed4677cc722cdbf98844707515f3ff0c54
mBabbitt <meganbabbitt23@gmail.com>
2015-06-20 06:57:00 -0700

    Checkpoint 38 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,15 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post =Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    @post=Post.new
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -27,14 +27,13 @@ class PostsController < ApplicationController
   end
 
   def update
-     @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
-       flash[:notice] = "Post was updated."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
-   end
-
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[error]="There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
 end
