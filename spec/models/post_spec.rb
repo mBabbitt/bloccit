6ddb57033aba3323b8fac2408aa5c2a0a788a831
mBabbitt <meganbabbitt23@gmail.com>
2015-07-06 14:14:09 -0700

    checkpoint-49 almost

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,32 +1,33 @@
 require 'rails_helper'
 
-describe  Post  do 
+describe Post do 
   describe "vote methods" do
+
     before do
-      user =  User.new
-      topic = Topic.new
-      @post = Post.create(title: 'post title', body: 'post bodies must be pretty long')
-      3.times{@post.votes.create(value: 1)}
-      2.times{@post.votes.create(value: -1)}
+      @post = Post.create(title: 'post title', body: 'Post bodies must be pretty long.')
+      3.times { @post.votes.create(value: 1) }
+      2.times { @post.votes.create(value: -1) }
     end
-    
+
     describe '#up_votes' do
-      it "countes number of votes with value =1" do
-        expect(@post.up_votes).to eq(3)
+      it "counts the number of votes with value = 1" do
+        expect( @post.up_votes ).to eq(3)
       end
     end
 
     describe '#down_votes' do
-      it "coutes number of votes with value = -1" do
-        expect(@post.down_votes).to eq(2)
+      it "counts the number of votes with value = -1" do
+        expect( @post.down_votes ).to eq(2)
       end
     end
 
     describe '#points' do
       it "returns the sum of all down and up votes" do
-        expect(@post.points).to eq(1) # 3-2 =1
+        expect( @post.points ).to eq(1) # 3 - 2
       end
-    end  
+    end
+  end
+end
+  
 
-  end 
-end 
+  
