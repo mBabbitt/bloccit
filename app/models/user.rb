47ec86754d55063b752c640e5688e037cfffdb50
mBabbitt <meganbabbitt23@gmail.com>
2015-07-07 12:50:56 -0700

    checkpoint-51 up to styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
@@ -14,4 +15,7 @@ class User < ActiveRecord::Base
     role == 'moderator'
   end    
 
+  def favorited(post)
+    favorites.where(post_id: post.id).first
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe User do
+  
+  include TestFactories
+
+  describe "#favorited(post)" do 
+    xit "returns 'nil' if the user has not faverited the post" do 
+    end
+
+    xit "return the appropraite favorite if it exists" do 
+    end
+  end
+end
+
