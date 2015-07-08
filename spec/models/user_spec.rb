d6bd86c0d8bb83fae830ec84701a4fba703336d3
mBabbitt <meganbabbitt23@gmail.com>
2015-07-08 08:21:48 -0700

    assign-51 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,19 @@
-require 'rails_helper'
-
-describe User do
-  
+require 'rails_helper.rb'
+describe User do 
   include TestFactories
-
-  describe "#favorited(post)" do 
-    xit "returns 'nil' if the user has not faverited the post" do 
+  describe "#favorited(post)" do
+    before do 
+      @user = authenticated_user
+      @post = associated_post
     end
-
-    xit "return the appropraite favorite if it exists" do 
+    it "returns `nil` if the user has not favorited the post" do
+      expect( @user.favorited(@post) ).to eq(nil)
     end
+  
+   it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorited(@post) ).to eq(favorite)
+   end   
+  
   end
 end
-
