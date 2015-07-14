56772c4dd1ff21b4e7d31fbeba4c5e3e95cc9b9e
mBabbitt <meganbabbitt23@gmail.com>
2015-07-14 09:33:05 -0700

    checkpoint-55 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,6 @@
 class TopicsController < ApplicationController
+
+
   def index
     @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
     authorize @topics
@@ -10,53 +12,55 @@ class TopicsController < ApplicationController
   end
 
   def show
-    @topic = Topic.find(params[:id])
-    @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
-    authorize @topic
-  end
+     @topic = Topic.find(params[:id])
+     authorize @topic
+     @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
+
+   end
 
   def edit
     @topic = Topic.find(params[:id])
-      authorize @topic
+    authorize @topic
   end
 
   def create
-    @topic = Topic.new(topic_params)
+    
+    @topic = Topic.new(topics_params)
     authorize @topic
     if @topic.save
-      redirect_to @topic, notice: "Topic was saved successfully."
+      redirect_to @topic, notice: 'Topic was saved successfully.'
     else
-      flash[:error] = "Error creating topic. Please try again."
-      render :new
-    end
-  end
+    flash[:error] = "Error creating a topic. Try again"
+    render :new
+    end  
+  end 
 
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(topic_params)
-       redirect_to @topic
-    else
-       flash[:error] = "There was an error saving the topic. Please try again."
-       render :edit
-    end
-  end
+    if @topic.update_attributes(topics_params)
+      redirect_to @topic, notice: 'Topic was updated successfully'
+    else 
+      flash[:error] = 'Error updating a topic. Try again'
+    end  
+  end 
 
   def destroy
     @topic = Topic.find(params[:id])
     authorize @topic
     if @topic.destroy
-      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      flash[:notice] = "\"#{@topic.name}\"  was deleted successfully"
       redirect_to @topic
     else
-      flash[:error] = "There was error deleting the topic."
+      flash[:error]  = "There was an error deleting  the topic"
       render :show
-    end
-  end
+    end  
+  end  
 
   private
-  
-  def topic_params
+
+  def topics_params
     params.require(:topic).permit(:name, :description, :public)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+  
+
+  get 'comments/new'
 
   devise_for :users
-    resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
+  
 
-    resources :topics do
-      resources :posts, except: [:index]
-    end
+  resources :topics do 
+     resources :posts, except: [:index], controller: 'topics/posts'
+  end
 
-    resources :posts, only: [] do
-      resources :comments, only: [:create, :destroy]
-      resources :favorites, only: [:create, :destroy]
-    
+  resources :posts, only: [:index] do 
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
 
-      post '/up-vote' => 'votes#up_vote', as: :up_vote
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
-    end 
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
 
-  get 'about' => 'welcome#about'
-    root to: 'welcome#index'
+
+  get 'about'=>  'welcome#about'
+  root to: 'welcome#index'
+
 end
