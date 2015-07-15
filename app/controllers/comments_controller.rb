36e3b71b3482152222e320121ad4933a5106f225
mBabbitt <meganbabbitt23@gmail.com>
2015-07-15 06:52:48 -0700

    checkpoint-57 completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,38 @@
 class CommentsController < ApplicationController
   
+
   def create
-    @post = Posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     authorize @comment
-    
     if @comment.save
       flash[:notice] = "Comment was saved"
-      redirect_to [@topic, @post]
+      redirect_to  [@post.topic, @post]
     else
       flash[:error] = "There was an error saving a comment. Please try again"  
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
+
     end
   end
 
+
   def destroy
-     @post = Post.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
- 
-     authorize @comment
-     if @comment.destroy
-       flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
-     else
-       flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
-     end
-   end
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was deleted"
+    else
+      flash[:error] = "There was an error deleting a comment. please try again"  
+    end  
+
+    respond_to do |format|
+      format.html
+      format.js
+    end  
+
+  end  
   
   private 
 
